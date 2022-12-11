8ee4babebc9774e8da312ae6602c7432a160d73a
Maximilian Nowak <maximilian.nowak@hm.edu>
2022-12-11 21:03:47 +0100

    add readme file

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #build the modules
 import os
-from distutils.core import setup, Extension
 import numpy
+
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
 
 os.environ["CC"] = "g++"
 os.environ["CFLAGS"] = "-std=c++2a"
 
 setup(name='cpp2np', version='1.0', 
-      ext_modules=[
-        Extension('cpp2np', ['cpp2np.cpp'], include_dirs=[numpy.get_include()]
-            )])
+    ext_modules=[
+        Extension('cpp2np', ['cpp2np.cpp'], include_dirs=[numpy.get_include()])
+        ]
+)
