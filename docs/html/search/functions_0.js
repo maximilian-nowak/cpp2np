11c67cca30b23227d4203f9402c2f0358aea25ac
Maximilian Nowak <maximilian.nowak@hm.edu>
2022-12-28 00:47:13 +0100

    update doxygen

--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['cpp2np_5fdescr_12',['cpp2np_descr',['../cpp2np_8cpp.html#a8b1ee65b6b9f8a5e3cf2a49bffe80310',1,'cpp2np.cpp']]],
-  ['cpp2np_5fdoublearray22_13',['cpp2np_doublearray22',['../cpp2np_8cpp.html#a020d62778b84f9266ab920e942911cbc',1,'cpp2np.cpp']]],
-  ['cpp2np_5ffree_14',['cpp2np_free',['../cpp2np_8cpp.html#a9eb9de49fffad1ff7da918399220dafb',1,'cpp2np.cpp']]],
-  ['cpp2np_5fint32array22_15',['cpp2np_int32array22',['../cpp2np_8cpp.html#a24aaeab5b221c807b4d2b7febf922b9e',1,'cpp2np.cpp']]],
-  ['cpp2np_5fint64array22_16',['cpp2np_int64array22',['../cpp2np_8cpp.html#ad94e8ac68b8ac8d0d7a01f1aa57b6b63',1,'cpp2np.cpp']]],
+  ['cpp2np_5fc_5farr_5ff8_12',['cpp2np_c_arr_f8',['../cpp2np_8cpp.html#a136ae531b21865b88e0f33b92d16236e',1,'cpp2np.cpp']]],
+  ['cpp2np_5fc_5farr_5fi4_13',['cpp2np_c_arr_i4',['../cpp2np_8cpp.html#a7d3e5c23d89290c09a8e5bdf680b3033',1,'cpp2np.cpp']]],
+  ['cpp2np_5fc_5farr_5fi8_14',['cpp2np_c_arr_i8',['../cpp2np_8cpp.html#a4239734cef98aa295c18030f01873c89',1,'cpp2np.cpp']]],
+  ['cpp2np_5fdescr_15',['cpp2np_descr',['../cpp2np_8cpp.html#a8b1ee65b6b9f8a5e3cf2a49bffe80310',1,'cpp2np.cpp']]],
+  ['cpp2np_5ffree_16',['cpp2np_free',['../cpp2np_8cpp.html#a9eb9de49fffad1ff7da918399220dafb',1,'cpp2np.cpp']]],
   ['cpp2np_5fwrap_17',['cpp2np_wrap',['../cpp2np_8cpp.html#ad516189b4963ba92b3b33f3f6eb0248b',1,'cpp2np.cpp']]]
 ];
